59b9e6414db991b8f7f0f846a10149e78189fcd8
Tadashi Nemoto <tadashi.nemoto0713@gmail.com>
2021-11-20 22:31:07 +0900

    init

---
+++ CircleCIDemo/CircleCIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircleCIDemoApp.swift
+//  CircleCIDemo
+//
+//  Created by Tadashi Nemoto on 2021/11/20.
+//
+
+import SwiftUI
+
+@main
+struct CircleCIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
